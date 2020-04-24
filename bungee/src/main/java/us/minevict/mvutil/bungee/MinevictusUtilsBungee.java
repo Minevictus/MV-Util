71557ab485c2d9150cdf650dc14e964277835965
Mariell Hoversholm <proximyst@proximyst.com>
2020-04-24 14:18:15 +0200

    :sparkles: Load ACF language file

--- bungee/src/main/java/us/minevict/mvutil/bungee/MinevictusUtilsBungee.java
+++ bungee/src/main/java/us/minevict/mvutil/bungee/MinevictusUtilsBungee.java
@@ -1,9 +1,15 @@
 package us.minevict.mvutil.bungee;
 
 import co.aikar.commands.BungeeCommandManager;
+import co.aikar.commands.MessageType;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.logging.Logger;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import us.minevict.mvutil.bungee.utils.Functions;
 import us.minevict.mvutil.common.MinevictusUtilsPlatform;
 
 public class MinevictusUtilsBungee
@@ -53,6 +59,33 @@ public class MinevictusUtilsBungee
    */
   @NotNull
   public BungeeCommandManager prepareAcf(@NotNull BungeeCommandManager commandManager) {
+    // Why the FUCK is MessageType not an enum??
+    for (var type : new MessageType[]{
+        MessageType.ERROR,
+        MessageType.HELP,
+        MessageType.INFO,
+        MessageType.SYNTAX
+    }) {
+      for (var colour : ChatColor.values()) {
+        commandManager.setFormat(type, colour.ordinal(), colour);
+      }
+    }
+
+    var acfLangFile = new File(commandManager.getPlugin().getDataFolder(), "acf-lang-en.yml");
+    try {
+      Functions.copyResource(
+          this,
+          "acf-lang-en.yml",
+          acfLangFile
+      );
+
+      commandManager.getLocales().loadYamlLanguageFile(acfLangFile, Locale.ENGLISH);
+    } catch (IOException ex) {
+      getLogger().warning("Could not save & load ACF language file for "
+          + commandManager.getPlugin().getDescription().getName());
+      ex.printStackTrace();
+    }
+
     return commandManager;
   }
 }
--- spigot/src/main/java/us/minevict/mvutil/spigot/MinevictusUtilsSpigot.java
+++ spigot/src/main/java/us/minevict/mvutil/spigot/MinevictusUtilsSpigot.java
@@ -1,8 +1,13 @@
 package us.minevict.mvutil.spigot;
 
+import co.aikar.commands.MessageType;
 import co.aikar.commands.PaperCommandManager;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.logging.Logger;
 import me.tom.sparse.spigot.chat.menu.ChatMenuAPI;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -10,6 +15,7 @@ import us.minevict.mvutil.common.MinevictusUtilsPlatform;
 import us.minevict.mvutil.spigot.hidden.CloseDatabaseListener;
 import us.minevict.mvutil.spigot.hidden.MegaChunkSizes;
 import us.minevict.mvutil.spigot.permissions.PermissionsManager;
+import us.minevict.mvutil.spigot.utils.Functions;
 
 public class MinevictusUtilsSpigot
     extends JavaPlugin
@@ -31,9 +37,12 @@ public class MinevictusUtilsSpigot
     getServer().getPluginManager().registerEvents(permissionsManager, this);
     getServer().getPluginManager().registerEvents(CloseDatabaseListener.getInstance(), this);
 
-    MegaChunkSizes.MEGA_CHUNK_SIZE = getConfig().getInt("megachunk-size", MegaChunkSizes.MEGA_CHUNK_SIZE);
-    MegaChunkSizes.MEGA_CHUNK_OFFSET_X = getConfig().getInt("megachunk-offset-x", MegaChunkSizes.MEGA_CHUNK_OFFSET_X);
-    MegaChunkSizes.MEGA_CHUNK_OFFSET_Z = getConfig().getInt("megachunk-offset-z", MegaChunkSizes.MEGA_CHUNK_OFFSET_Z);
+    MegaChunkSizes.MEGA_CHUNK_SIZE = getConfig()
+        .getInt("megachunk-size", MegaChunkSizes.MEGA_CHUNK_SIZE);
+    MegaChunkSizes.MEGA_CHUNK_OFFSET_X = getConfig()
+        .getInt("megachunk-offset-x", MegaChunkSizes.MEGA_CHUNK_OFFSET_X);
+    MegaChunkSizes.MEGA_CHUNK_OFFSET_Z = getConfig()
+        .getInt("megachunk-offset-z", MegaChunkSizes.MEGA_CHUNK_OFFSET_Z);
 
     ChatMenuAPI.init(this);
   }
@@ -80,6 +89,32 @@ public class MinevictusUtilsSpigot
    */
   @NotNull
   public PaperCommandManager prepareAcf(@NotNull PaperCommandManager commandManager) {
+    // Why the FUCK is MessageType not an enum??
+    for (var type : new MessageType[]{
+        MessageType.ERROR,
+        MessageType.HELP,
+        MessageType.INFO,
+        MessageType.SYNTAX
+    }) {
+      for (var colour : org.bukkit.ChatColor.values()) {
+        commandManager.setFormat(type, colour.ordinal(), colour);
+      }
+    }
+
+    var acfLangFile = new File(commandManager.getPlugin().getDataFolder(), "acf-lang-en.yml");
+    try {
+      Functions.copyResource(
+          this,
+          "acf-lang-en.yml",
+          acfLangFile
+      );
+
+      commandManager.getLocales().loadYamlLanguageFile(acfLangFile, Locale.ENGLISH);
+    } catch (IOException | InvalidConfigurationException ex) {
+      getLogger().warning("Could not save & load ACF language file for "
+          + commandManager.getPlugin().getDescription().getName());
+      ex.printStackTrace();
+    }
 
     return commandManager;
   }
