7931149e7600c60fc4b7062578d2b027c12a1633
Mariell Hoversholm <proximyst@proximyst.com>
2020-05-03 19:35:19 +0200

    :bulb: Documentation for Platformless

--- common/src/main/java/us/minevict/mvutil/common/utils/Platformless.java
+++ common/src/main/java/us/minevict/mvutil/common/utils/Platformless.java
@@ -10,8 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import us.minevict.mvutil.common.text.ComponentBuilderV2;
 
 /**
- * Functions which do not require any specific platform while still doing something specific to
- * each.
+ * Functions which do not require any specific platform while still doing something specific to each.
  *
  * @since 3.6.0
  */
@@ -23,16 +22,35 @@ public final class Platformless {
   static BiConsumer<CommandIssuer, BaseComponent[]> messageSender = null;
   static Function<Supplier, CompletableFuture> asyncRunner = null;
 
+  /**
+   * Send a message to the {@link CommandIssuer} regardless of platform.
+   *
+   * @param issuer     The issuer to receive a message.
+   * @param components The components to send.
+   */
   public static void sendMessage(@NotNull CommandIssuer issuer,
       @NotNull BaseComponent[] components) {
     messageSender.accept(issuer, components);
   }
 
+  /**
+   * Send a message to the {@link CommandIssuer} regardless of platform.
+   *
+   * @param issuer  The issuer to receive a message.
+   * @param builder The {@link ComponentBuilderV2} to send.
+   */
   public static void sendMessage(@NotNull CommandIssuer issuer,
       @NotNull ComponentBuilderV2 builder) {
     sendMessage(issuer, builder.create());
   }
 
+  /**
+   * Run a given {@link Supplier} asynchronously.
+   *
+   * @param runnable The {@link Supplier} to run elsewhere.
+   * @param <T>      The type to return in the {@link CompletableFuture}.
+   * @return A {@link CompletableFuture} with the result from the runnable or its exception if it threw any.
+   */
   @SuppressWarnings("unchecked")
   public static <T> CompletableFuture<T> runAsync(Supplier<T> runnable) {
     return (CompletableFuture<T>) asyncRunner.apply(runnable);
